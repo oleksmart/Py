f125dab01e62c384b4dcc4c4f5e7d90b4cae9b30
Oleks Mart <alx.woland@gmail.com>
2024-06-07 15:25:16 +0300

    07-06

--- test17-gui-archiver.py
+++ test17-gui-archiver.py
@@ -1,17 +1,31 @@
 import FreeSimpleGUI as sg
+from zip_creator import make_archive
+import unittest
 
 label1 = sg.Text("Select files to compress:")
 input1 = sg.Input()
-choose_button1 = sg.FileBrowse("Chose")
+choose_button1 = sg.FilesBrowse("Chose", key="files")
 
 label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Chose")
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
+
+output = sg.Text(key="output", text_color="yellow")
+
 window = sg.Window("File Compressor",
-                    layout=[[label1, input1], 
-                            [label2, input2],
-                            [choose_button2]])
-window.read()
+                    layout=[[label1, input1, choose_button1], 
+                            [label2, input2, choose_button2],
+                            [compress_button, output]])
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    print(filepaths , folder)
+    make_archive(filepaths,folder)
+    window["output"].update(value="Compression completed!")
+
+
 window.close()
--- test17day-gui.py
+++ test17day-gui.py
@@ -7,10 +7,13 @@ add_button = sg.Button(button_text='Add')
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', 
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
-
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("EXIT")
 window = sg.Window('MY to-do App', 
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]], 
+                   layout=[[label],
+                           [input_box, add_button], 
+                           [list_box, edit_button], [complete_button],
+                           [exit_button]], 
                    font=('Helvetica', 12))
 while True:
     event, values = window.read()
@@ -24,19 +27,29 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
+
             todos= functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos= functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
         case 'todos':
             window['todo'].update(value = values['todos'][0])
+        case 'EXIT':
+            break
+            
         case sg.WIN_CLOSED:
             break
-
-event = window.read()
 
 window.close()
