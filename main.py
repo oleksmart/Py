14d3c94d67f39a0479bcf1f4a0bb46f4a047ee55
Oleks Mart <alx.woland@gmail.com>
2024-06-21 16:51:16 +0300

    1

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import functions 
+
+
+todos = functions.get_todos()
+
+st.set_page_config(layout="wide") 
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My todo app")
+st.subheader("This is my todo app")
+st.write("This <b>is text</b>", unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add new todo...", 
+              on_change=add_todo, key='new_todo')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
+
+
+st.session_state
